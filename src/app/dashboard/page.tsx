351a2bc9dd99c6601eea1e85b4d114d6a210b400
Netz <nilmar2024@gmail.com>
2024-09-06 21:17:05 -0600

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Dashboard } from '@/components/component/dashboard'
+import React from 'react'
+
+const DashboardL = () => {
+  return (
+    <Dashboard />
+  )
+}
+
+export default DashboardL
